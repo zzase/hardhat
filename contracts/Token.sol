bd133610f7f32d5b4885fee88d8a4fc6d18a5032
zzase <sehhon9867@naver.com>
2021-12-22 10:47:28 +0900

    make simple token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.4;
+
+contract Token {
+
+    string public name = "My hardhat Token";
+    string public symbol = "ZST";
+
+    uint256 public totalSupply = 100000;
+
+    address public owner;
+
+    mapping(address => uint256) balances;
+
+    constructor() {
+        balances[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint256 amount) external {
+        require(balances[msg.sender] >= amount, "Not enough tokens");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return balances[account];
+    }
+}
